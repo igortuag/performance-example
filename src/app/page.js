881c2192032a74ecf5ff7cae2dbb71838550a0e5
Tuag <igortuag@gmail.com>
2024-07-22 08:41:37 -0300

    add default loading example

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,14 @@
 const getUsers = async () => {
+  // settimeout is just to simulate a slow request
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const users = await res.json();
   return users;
+};
+
+const Loading = () => {
+  return <div>Loading...</div>;
 };
 
 export default async function Home() {
